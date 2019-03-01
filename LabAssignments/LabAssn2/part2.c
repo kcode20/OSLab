906327a219610406ef891bae270a6157bb368798
Khristian Brooks <kh.brooks02@gmail.com>
2019-03-01 15:34:21 -0500

    update homework 2

--- LabAssignments/LabAssn2/part2.c
+++ LabAssignments/LabAssn2/part2.c
@@ -17,18 +17,18 @@ int main(){
   fq=fork(); // fork a child - call it Process Q
   if(fq==0){// Child successfully forked
     a=a+b;
-    printf("a: %d, b: %d, process_id:%d\n", a, b, getpid());//values of a, b, and process_id
+    printf("a: %d, b: %d, process_id:%d, parent_id:%d\n", a, b, getpid(), getppid());//values of a, b, and process_id
     fr=fork(); // fork another child - call it Process R
     if(fr!=0){
       b=b+20;
-      printf("a: %d, b: %d, process_id:%d\n", a, b, getpid()); // values of a, b, and process_id
+      printf("a: %d, b: %d, process_id:%d, parent_id:%d\n", a, b, getpid(), getppid());; // values of a, b, and process_id
     }
     else{
       a=(a*b)+30;
-      printf("a: %d, b: %d, process_id:%d\n", a, b, getpid()); //values of a, b, and process_id
+      printf("a: %d, b: %d, process_id:%d, parent_id:%d\n", a, b, getpid(), getppid()); //values of a, b, and process_id
     }
   } 
   else
   b=a+b-5;
-  printf("a: %d, b: %d, process_id:%d\n", a, b, getpid()); // values of a, b, and process_id
+  printf("a: %d, b: %d, process_id:%d, parent_id:%d\n", a, b, getpid(), getppid()); // values of a, b, and process_id
 }
